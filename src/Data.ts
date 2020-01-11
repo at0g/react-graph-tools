c59cbf3a567b1aa2bdc671ef7214cb036432ea1d
sinclairzx81 <haydn.developer@gmail.com>
2020-01-12 10:14:46 +1300

    computed layout | edges

--- src/Data.ts
+++ src/Data.ts
@@ -1,8 +1,8 @@
 /** Top level schema for the graph: Can change this to whatever. */
 export interface Layout {
-    x:      number
-    y:      number
-    width:  number
+    x: number
+    y: number
+    width: number
     height: number
     zIndex: number
 }
@@ -15,14 +15,14 @@ export interface Port {
     name: string
 }
 export interface Node {
-    id:     string
-    name:   string
-    ports:  Port[]
+    id: string
+    name: string
+    ports: Port[]
     layout: Layout
 }
 export interface Edge {
     from: [string, string]
-    to:   [string, string]
+    to: [string, string]
 }
 export interface Graph {
     layout: GraphLayout
@@ -33,70 +33,82 @@ export interface Graph {
 /** Generates a Basic Starter Graph */
 export const createGraphData = (): Graph => ({
     layout: {
-        x:      0, 
-        y:      0, 
-        width:  216, 
-        height: 100, 
+        x: 0,
+        y: 0,
+        width: 216,
+        height: 100,
         zIndex: 0,
         scale: 1
     },
     nodes: [
         {
-            id:   'A',
+            id: 'Foo',
             name: 'Foo',
-            ports:  [
-                { type: 'input',  name: 'input_a'}, 
-                { type: 'output',  name: 'output_b'}, 
+            ports: [
+                { type: 'input', name: 'input_a' },
+                { type: 'output', name: 'output_b' },
             ],
-            layout: { 
-                x:      10, 
-                y:      10, 
-                width:  216, 
-                height: 100, 
-                zIndex: 0 
+            layout: {
+                "x": 31,
+                "y": 444,
+                "width": 216,
+                "height": 100,
+                "zIndex": 61
             },
         },
         {
-            id:   'B',
+            id: 'Bar',
             name: 'Bar',
-            ports:  [
-                { type: 'input',  name: 'input_a'}, 
-                { type: 'input',  name: 'input_b'}, 
-                { type: 'input',  name: 'input_c'}, 
-                { type: 'output',  name: 'output_d'}, 
-                { type: 'output',  name: 'output_e'}, 
-                { type: 'output',  name: 'output_f'}, 
+            ports: [
+                { type: 'input', name: 'input_a' },
+                { type: 'input', name: 'input_b' },
+                { type: 'input', name: 'input_c' },
+                { type: 'output', name: 'output_d' },
+                { type: 'output', name: 'output_e' },
+                { type: 'output', name: 'output_f' },
             ],
-            layout: { 
-                x:      250, 
-                y:      10, 
-                width:  256, 
-                height: 100, 
-                zIndex: 1
+            layout: {
+                "x": 700,
+                "y": 133,
+                "width": 256,
+                "height": 100,
+                "zIndex": 57
             },
         },
         {
-            id:   'C',
+            id: 'Baz',
             name: 'Baz',
-            ports:  [
-                { type: 'input',  name: 'input_a'}, 
-                { type: 'output',  name: 'output_b'}, 
+            ports: [
+                { type: 'input', name: 'input_a' },
+                { type: 'output', name: 'output_b' },
             ],
-            layout: { 
-                width:  256,
-                height: 100,
-                x:      210, 
-                y:      210, 
-                zIndex: 2 
+            layout: {
+                "width": 256,
+                "height": 100,
+                "x": 348,
+                "y": 30,
+                "zIndex": 60
+            },
+        },
+        {
+            id: 'Qux',
+            name: 'Qux',
+            ports: [
+                { type: 'input', name: 'input_a' },
+                { type: 'output', name: 'output_b' },
+            ],
+            layout: {
+                "width": 256,
+                "height": 100,
+                "x": 344,
+                "y": -261,
+                "zIndex": 63
             },
         }
     ],
     edges: [
-        { from: ['A', 'a'], to: ['B', 'a'] },
-        { from: ['A', 'a'], to: ['B', 'b'] },
-        { from: ['A', 'a'], to: ['B', 'c'] },
-        { from: ['A', 'b'], to: ['C', 'a'] },
-        { from: ['A', 'b'], to: ['C', 'b'] },
-        { from: ['A', 'b'], to: ['C', 'c'] },
+        { from: ['Foo', 'output_b'], to: ['Baz', 'input_a'] },
+        { from: ['Baz', 'output_b'], to: ['Bar', 'input_c'] },
+        { from: ['Qux', 'output_b'], to: ['Bar', 'input_b'] },
     ]
 })
--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,20 +1,30 @@
 import './Graph.css'
-import React                 from 'react'
+import React from 'react'
 import { Graph, Node, Port } from './Data'
+import { rootCertificates } from 'tls'
 
 // ------------------------------------------------------------------
 // Port Elements | Input | Output
 // ------------------------------------------------------------------
 
 export interface InputPortElementProperties {
-    onInputMouseUp: (port: Port, event: React.MouseEvent) => void
+    onConnectorLayout: (node: Node, port: Port, rect: DOMRect) => void
+    onInputMouseUp: (node: Node, port: Port, event: React.MouseEvent) => void
+    node: Node
     port: Port
 }
 export function InputPortElement(props: InputPortElementProperties) {
+    const connector = React.useRef<HTMLDivElement>(null)
+
+    React.useLayoutEffect(() => {
+        const rect = connector.current!.getBoundingClientRect()!
+        props.onConnectorLayout(props.node, props.port, rect)
+    })
+
     return <div className='port'>
         <div className='port-left'>
-            <div className='port-connector'
-                onMouseUp={e => props.onInputMouseUp(props.port, e)}
+            <div className='port-connector' ref={connector}
+                onMouseUp={e => props.onInputMouseUp(props.node, props.port, e)}
             ></div>
         </div>
         <div className='port-middle noselect'>{props.port.name}</div>
@@ -23,16 +33,25 @@ export function InputPortElement(props: InputPortElementProperties) {
 }
 
 export interface OutputPortElementProperties {
-    onOutputMouseDown: (port: Port, event: React.MouseEvent) => void
+    onOutputMouseDown: (node: Node, port: Port, event: React.MouseEvent) => void
+    onConnectorLayout: (node: Node, port: Port, rect: DOMRect) => void
+    node: Node
     port: Port
 }
 export function OutputPortElement(props: OutputPortElementProperties) {
+    const connector = React.useRef<HTMLDivElement>(null)
+
+    React.useLayoutEffect(() => {
+        const rect = connector.current!.getBoundingClientRect()!
+        props.onConnectorLayout(props.node, props.port, rect)
+    })
+
     return <div className='port'>
         <div className='port-left'></div>
         <div className='port-middle noselect'>{props.port.name}</div>
         <div className='port-right'>
-            <div className='port-connector'
-                onMouseDown={e => props.onOutputMouseDown(props.port, e)}
+            <div className='port-connector' ref={connector}
+                onMouseDown={e => props.onOutputMouseDown(props.node, props.port, e)}
             ></div>
         </div>
     </div>
@@ -46,25 +65,40 @@ export interface NodeElementProperties {
     onNodeHeaderMouseDown: (node: Node, event: React.MouseEvent) => void,
     onInputMouseUp:        (node: Node, port: Port, event: React.MouseEvent) => void
     onOutputMouseDown:     (node: Node, port: Port, event: React.MouseEvent) => void
+    onConnectorLayout:     (node: Node, port: Port, rect:  DOMRect) => void
     graph: Graph,
-    node:  Node
+    node: Node
 }
 export function NodeElement(props: NodeElementProperties) {
-
     const ports = props.node.ports.map(port => {
-        switch(port.type) {
-            case 'input':  return <InputPortElement  key={port.name} port={port} onInputMouseUp   ={(port, event) => props.onInputMouseUp(props.node, port, event)} />
-            case 'output': return <OutputPortElement key={port.name} port={port} onOutputMouseDown={(port, event) => props.onOutputMouseDown(props.node, port, event)} />
+        switch (port.type) {
+            case 'input': return (
+                <InputPortElement
+                    key={port.name}
+                    onConnectorLayout={props.onConnectorLayout}
+                    onInputMouseUp={props.onInputMouseUp}
+                    node={props.node}
+                    port={port}
+                />
+            )
+            case 'output': return (
+                <OutputPortElement
+                    key={port.name}
+                    onConnectorLayout={props.onConnectorLayout}
+                    onOutputMouseDown={props.onOutputMouseDown}
+                    node={props.node}
+                    port={port}
+                />
+            )
             default: return null
         }
     })
-
     return <div className='node' style={{
         transform: `translate(${props.node.layout.x}px, ${props.node.layout.y}px)`,
-        width:     props.node.layout.width,
-        zIndex:    props.node.layout.zIndex,
+        width: props.node.layout.width,
+        zIndex: props.node.layout.zIndex,
     }}>
-        <div className='node-header noselect' onMouseDown= {e => props.onNodeHeaderMouseDown(props.node, e)}>
+        <div className='node-header noselect' onMouseDown={e => props.onNodeHeaderMouseDown(props.node, e)}>
             {props.node.name}
         </div>
         <div className='node-body'>
@@ -78,7 +112,13 @@ export function NodeElement(props: NodeElementProperties) {
 // Graph Element
 // ------------------------------------------------------------------
 
+export interface Line {
+    from: { x: number, y: number }
+    to:   { x: number, y: number }
+}
+
 export interface GraphElementState {
+    revision: number, 
     graph: Graph
     draggable: {
         target: Node | Graph | null
@@ -86,8 +126,9 @@ export interface GraphElementState {
     connector: {
         active: boolean
         from: { x: number, y: number }
-        to:   { x: number, y: number }
-    }
+        to: { x: number, y: number }
+    },
+    lines: Line[]
 }
 export interface GraphElementProperties {
     onGraphChange: (graph: Graph) => void
@@ -95,7 +136,9 @@ export interface GraphElementProperties {
 }
 export function GraphElement(props: GraphElementProperties) {
     const [state, _setState] = React.useState<GraphElementState>({
+        revision: 0,
         graph: { ...props.graph },
+        lines: [],
         draggable: {
             target: null
         },
@@ -103,17 +146,48 @@ export function GraphElement(props: GraphElementProperties) {
             active: false,
             from: { x: 0, y: 0 },
             to:   { x: 0, y: 0 }
-        }
+        },
     })
+
+    const accumulator: Array<{node: Node, port: Port, rect: DOMRect}> = []
+    function onConnectorLayout(node: Node, port: Port, rect: DOMRect) {
+        accumulator.push({node, port, rect})
+    }
+
+    React.useLayoutEffect(() => {
+        const lines: Line[] = []
+        for(const edge of state.graph.edges) {
+            const from = accumulator.find(({node, port}) => node.id === edge.from[0] && port.name === edge.from[1])!
+            const to   = accumulator.find(({node, port}) => node.id === edge.to[0]   && port.name === edge.to[1])!
+            const _scale = (window.devicePixelRatio * state.graph.layout.scale)
+            // need to transform the SVG lines by the current translate and scale.
+            // scale is utterly broken in the given setup.
+            //
+            // Using CSS transforms, we transform from the 'top left' which makes 
+            // computing the scale extremely awkward, consider centering origin 
+            // at element midpoint (w/2, h/2) and transforming from there.
+            lines.push({
+                from: { 
+                    x: ((from.rect.x + from.rect.width / 2) - state.graph.layout.x), 
+                    y: ((from.rect.y + from.rect.height / 2) - state.graph.layout.y), 
+                }, 
+                to: { 
+                    x: ((to.rect.x + to.rect.width  / 2) - state.graph.layout.x), 
+                    y: ((to.rect.y + to.rect.height / 2) - state.graph.layout.y), 
+                }, 
+            })
+        }
+        setState({ ...state, lines })
+    }, [state.revision])
 
     // Intercept all state changes and emit updated state.
     function setState(state: GraphElementState) {
-        props.onGraphChange({...state.graph})
+        props.onGraphChange({ ...state.graph })
         _setState(state)
     }
 
-    function onMouseMove (e: React.MouseEvent) {
-        if(state.draggable.target !== null) {
+    function onMouseMove(e: React.MouseEvent) {
+        if (state.draggable.target !== null) {
             // note: calculate the actual delta from the device pixel ratio and 
             // the current scale of the graph layout. We shift the target by the
             // given deltas. (Better than tracking via clientXY positions.)
@@ -121,31 +195,33 @@ export function GraphElement(props: GraphElementProperties) {
             const deltaY = e.movementY / (window.devicePixelRatio * state.graph.layout.scale)
             state.draggable.target.layout.x += deltaX
             state.draggable.target.layout.y += deltaY
+            state.revision += 1
             setState({ ...state })
         }
 
-        if(state.connector.active) {
+        if (state.connector.active) {
             const deltaX = e.movementX / (window.devicePixelRatio * state.graph.layout.scale)
             const deltaY = e.movementY / (window.devicePixelRatio * state.graph.layout.scale)
             state.connector.to.x += deltaX
             state.connector.to.y += deltaY
+            state.revision += 1
             setState({ ...state })
         }
     }
 
-    function onMouseDown (e: React.MouseEvent) {
+    function onMouseDown(e: React.MouseEvent) {
         e.stopPropagation()
         state.draggable.target = state.graph
-        setState({ ...state })   
+        setState({ ...state })
     }
 
-    function onMouseUp (e: React.MouseEvent) {
+    function onMouseUp(e: React.MouseEvent) {
         state.draggable.target = null
         state.connector.active = false
         setState({ ...state })
     }
-    
-    function onNodeHeaderMouseDown (node: Node, e: React.MouseEvent) {
+
+    function onNodeHeaderMouseDown(node: Node, e: React.MouseEvent) {
         e.stopPropagation()
         const top = state.graph.nodes.reduce((acc, node) => node.layout.zIndex > acc ? node.layout.zIndex : acc, 0)
         node.layout.zIndex = (top + 1)
@@ -154,50 +230,59 @@ export function GraphElement(props: GraphElementProperties) {
     }
 
     function onNodeInputMouseUp(node: Node, port: Port, e: React.MouseEvent) {
-        console.log('input: mouseup')
-        console.log(node, port)
     }
 
     function onNodeOutputMouseDown(node: Node, port: Port, e: React.MouseEvent) {
-        e.stopPropagation()        
+        e.stopPropagation()
         state.connector.from.x = e.clientX
         state.connector.from.y = e.clientY
-        state.connector.to.x   = e.clientX
-        state.connector.to.y   = e.clientY
+        state.connector.to.x = e.clientX
+        state.connector.to.y = e.clientY
         state.connector.active = true
-        setState({...state})
+        setState({ ...state })
     }
 
     function onWheel(e: React.WheelEvent) {
         const current = state.graph.layout.scale
-        const delta   = e.deltaY > 1 ? -0.25 : 0.25
+        const delta = e.deltaY > 1 ? -0.25 : 0.25
         state.graph.layout.scale = (current + delta <= 0) ? current : current + delta
-        setState({ ...state})
+        setState({ ...state })
     }
 
     const connector = state.connector.active ? (
-        <svg style={{width: '100%', height: '100%', position: 'fixed'}} xmlns="http://www.w3.org/2000/svg">
-            <line x1={state.connector.from.x} y1={state.connector.from.y} x2={state.connector.to.x} y2={state.connector.to.y} style={{stroke:'#333', strokeWidth:2 }} />
+        <svg style={{ width: '100%', height: '100%', position: 'fixed' }} xmlns="http://www.w3.org/2000/svg">
+            <line x1={state.connector.from.x} y1={state.connector.from.y} x2={state.connector.to.x} y2={state.connector.to.y} style={{ stroke: '#333', strokeWidth: 2 }} />
         </svg>
     ) : null;
 
     const nodes = state.graph.nodes.map(node => (
         <NodeElement key={node.id} graph={state.graph} node={node}
-            onNodeHeaderMouseDown={onNodeHeaderMouseDown}    
+            onConnectorLayout={onConnectorLayout}
+            onNodeHeaderMouseDown={onNodeHeaderMouseDown}
             onInputMouseUp={onNodeInputMouseUp}
             onOutputMouseDown={onNodeOutputMouseDown}
         />
     ))
+
+    const edges = state.lines.map(line => (
+        <line x1={line.from.x} 
+              y1={line.from.y} 
+              x2={line.to.x} 
+              y2={line.to.y} 
+              style={{ stroke: '#333', strokeWidth: 2 }} />)
+    )
+    
     return <div className='graph'
-        onMouseMove={onMouseMove}    
+        onMouseMove={onMouseMove}
         onMouseDown={onMouseDown}
         onMouseUp={onMouseUp}
         onWheel={onWheel}>
-        <div className='graph-scale' style={{transform: `scale(${state.graph.layout.scale})`}}>
-            <div className='graph-translate' style={{transform: `translate(${state.graph.layout.x}px, ${state.graph.layout.y}px)`}}>
-                {connector}
-                {nodes}
-            </div>
+        <div className='graph-transform' style={{ transform: `translate(${state.graph.layout.x}px, ${state.graph.layout.y}px) scale(${state.graph.layout.scale})` }}>
+            <svg style={{ width: '100%', height: '100%', position: 'fixed' }} xmlns="http://www.w3.org/2000/svg">
+                {edges}
+            </svg>
+            {connector}
+            {nodes}
         </div>
     </div>
 }
